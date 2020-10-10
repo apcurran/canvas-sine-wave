00ee657ed0efe3a7a50f6c94bfe364fd7bc36f28
Alex Curran <alexcurran.va@gmail.com>
2020-10-10 13:45:08 -0500

    Removed console.log

--- myWorker.js
+++ myWorker.js
@@ -1,19 +1,8 @@
 "use strict";
 
 onmessage = function(event) {
-    console.log("Worker running...")
-    
     const canvas = event.data.canvas;
     const ctx = canvas.getContext("2d");
-    
-    // if (event.data.width) {
-    //     state.width = event.data.width;
-    //     canvas.height = event.data.height;
-    // }
-    // Call to resize canvas
-    // canvas.width = state.width; 
-    // canvas.height = state.height;
-    
     const wave = {
         y: canvas.height / 2,
         length: .01,
@@ -37,9 +26,11 @@ onmessage = function(event) {
     
         ctx.beginPath();
         ctx.moveTo(0, canvas.height / 2);
+
         for (let i = 0; i < canvas.width; i++) {
             ctx.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude * Math.sin(increment));
         }
+
         ctx.strokeStyle = `hsl(${Math.abs(strokeColor.h * Math.sin(increment))}, ${Math.abs(strokeColor.s * Math.sin(increment))}%, ${strokeColor.l}%)`;
         ctx.stroke();
     
